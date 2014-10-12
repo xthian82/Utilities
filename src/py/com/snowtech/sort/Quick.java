10a43e1796f43d7f6cb1ecaba8532d88ae1b7da1
Cristhian Recalde <crecalde@gmail.com>
2014-10-12 01:00:37 -0400

    Quicksort

--- src/py/com/snowtech/sort/Quick.java
+++ src/py/com/snowtech/sort/Quick.java
@@ -1,36 +1,73 @@
 package py.com.snowtech.sort;
 
-import py.com.snowtech.util.Utils;
+import java.util.Comparator;
 
-public class Quick {
-	
-	public static void sort(int[] v, int m, int n) {
-		int i,j,k;
+@SuppressWarnings("rawtypes")
+public class Quick extends Sort {
+	private void sort(Comparable[] v, int m, int n) {
+		int i,j;
+		Comparable k;
 		if (m < n) {
 			i = m;
 			j = n+1;
 			k = v[i];
+			
 			while (true)
 			{
-				do {++i;} while (i<v.length && v[i]<k);
-				do {--j;} while (j>-1 && v[j]>k);
+				do {++i;} while (i<v.length && less(v[i], k));
+				do {--j;} while (j>-1 && less(k, v[j]));
 
-				if (i<j)
-					Utils.swapElement(v, i, j);
+				if (i<j) 
+					exch(v, i, j);
 				else
 					break;
 			}
-			Utils.swapElement(v, m, j);
+			
+			exch(v, m, j);
 			sort(v, m, j-1);
 			sort(v, j+1, n);
 		}
 	}
 	
+	private void sort(Comparator c, Object[] v, int m, int n) {
+		int i,j;
+		Object k;
+		if (m < n) {
+			i = m;
+			j = n+1;
+			k = v[i];
+			
+			while (true)
+			{
+				do {++i;} while (i<v.length && less(c, v[i], k));
+				do {--j;} while (j>-1 && less(c, k, v[j]));
+
+				if (i<j) 
+					exch(v, i, j);
+				else
+					break;
+			}
+			
+			exch(v, m, j);
+			sort(c, v, m, j-1);
+			sort(c, v, j+1, n);
+		}
+	}
+	
+	public void sort(Comparable[] o) {
+		sort(o, 0, o.length-1);
+	}
+	
+	public void sort(Object[] o, Comparator c) {
+		sort(c, o, 0, o.length-1);
+	}
+	
 	public static void main(String[] args) {
-		int [] a = {6,5,46,2,3,10,9};
+		Integer [] a = {6,5,46,2,3,10,9};
+		Quick quick = new Quick();
 		
-		Utils.print(a);
-		Quick.sort(a, 0, a.length-1);
-		Utils.print(a);
+		quick.print(a);
+		quick.sort(a);
+		quick.print(a);
 	}
 }
